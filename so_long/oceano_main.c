abfbad2c4995cdd85839d2603dfea6fbf2de9e29
vivaz-ca | HOME <insanecoderbr06@gmail.com>
2025-05-18 21:57:33 +0100

    Concepts and ideas of minilibx more established

--- so_long/oceano_main.c
+++ so_long/oceano_main.c
@@ -1,21 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   oceano_main.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vivaz-ca <vivaz-ca@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/17 16:03:36 by vivaz-ca          #+#    #+#             */
-/*   Updated: 2025/05/18 21:55:26 by vivaz-ca         ###   ########.fr       */
+/*   Created: 2025/05/18 18:50:11 by vivaz-ca          #+#    #+#             */
+/*   Updated: 2025/05/18 21:25:20 by vivaz-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
-# include "minilibx-linux/mlx.h"
-# include <X11/keysym.h>
-# include <stdlib.h>
-# include <stdio.h>
+#include "so_long.h"
 
-#endif
+int main(void)
+{
+	void	*mlx_connection;
+	void	*mlx_window;
 
+	mlx_connection = mlx_init();
+	if (mlx_connection == NULL)
+		return (1);
+	mlx_window = mlx_new_window(mlx_connection, 800, 600, "oceano");
+	if (!mlx_window)
+	{
+		mlx_destroy_display(mlx_connection);
+		free(mlx_connection);
+		return (1);
+	}
+	mlx_loop(mlx_connection);
+	
+}
