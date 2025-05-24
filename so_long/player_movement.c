90fc5e8cd5443379ccc3678be03b7abe96d8d34b
vivaz-ca <vivaz-ca@student.42lisboa.com>
2025-05-24 18:40:22 +0100

    sla

--- so_long/player_movement.c
+++ so_long/player_movement.c
@@ -6,7 +6,7 @@
 /*   By: vivaz-ca <vivaz-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 18:11:05 by vivaz-ca          #+#    #+#             */
-/*   Updated: 2025/05/24 18:39:00 by vivaz-ca         ###   ########.fr       */
+/*   Updated: 2025/05/24 18:39:59 by vivaz-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,10 +42,14 @@ int keypress_to_walk(int keysym, void *param)
 	return (0);
 }
 
-int handle_exit(int keysym, void *param)
+int handle_exit(int keysym, t_mlx_data *data)
 {
-	if (keysym == 65307)
+	if (keysym == XK_Escape)
 	{
-		
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		mlx_destroy_display(data->mlx_ptr);
+		free(data->mlx_ptr);
+		exit(0);
 	}
+	return (0);
 }
