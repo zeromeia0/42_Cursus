9d80d8a905e90128ebe6d7a32244aa5956c084e5
vivaz-ca <vivaz-ca@student.42lisboa.com>
2025-05-24 18:17:20 +0100

    sla

--- so_long/player_movement.c
+++ so_long/player_movement.c
@@ -6,13 +6,26 @@
 /*   By: vivaz-ca <vivaz-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 18:11:05 by vivaz-ca          #+#    #+#             */
-/*   Updated: 2025/05/24 18:13:14 by vivaz-ca         ###   ########.fr       */
+/*   Updated: 2025/05/24 18:17:04 by vivaz-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	ft_player_movement(void)
+int keypress_to_walk(int keysym, void *param)
 {
-	
+	t_mlx_data *data = (t_mlx_data *)param;
+	t_gato *gato = so_long();
+
+	if (keysym == 119 || keysym == 65362) // w
+		gato->y -= 10;
+	else if (keysym == 97 || keysym == 65361) // a
+		gato->x -= 10;
+	else if (keysym == 115 || keysym == 65364) // s
+		gato->y += 10;
+	else if (keysym == 100 || keysym == 65363) // d
+		gato->x += 10;
+
+	printf("Key pressed: %d | Position -> x: %d, y: %d\n", keysym, gato->x, gato->y);
+	return (0);
 }
