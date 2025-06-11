319b76dc2da552f2eebb5472fb7ed71604bea42e
vivaz-ca | HOME <insanecoderbr06@gmail.com>
2025-06-11 08:47:35 +0100

    norminett flood fill not done

--- so_long/utils.c
+++ so_long/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vivaz-ca <vivaz-ca@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vivaz-ca <vivaz-ca@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 16:52:07 by vivaz-ca          #+#    #+#             */
-/*   Updated: 2025/06/09 13:19:23 by vivaz-ca         ###   ########.fr       */
+/*   Updated: 2025/06/11 08:45:41 by vivaz-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,70 +81,4 @@ void	*ft_calloc(size_t nmemb, size_t size)
 		return (NULL);
 	ft_bzero(ptr, nmemb * size);
 	return (ptr);
-}
-
-char	**realloc_map(char **old_map, int old_size, int new_size)
-{
-	char	**new_map;
-	int		i;
-
-	new_map = ft_calloc(sizeof(char *), (new_size + 1));
-	if (!new_map)
-		return (NULL);
-	i = 0;
-	while (i < old_size)
-	{
-		new_map[i] = old_map[i];
-		i++;
-	}
-	new_map[i] = NULL;
-	free(old_map);
-	return (new_map);
-}
-
-int get_map_height(char **map)
-{
-	t_create_map *map_data;
-	int height;
-
-	map_data = ft_calloc(1, sizeof(t_create_map));
-	if (!map_data)
-		return (0);
-	while (map[map_data->map_height])
-		map_data->map_height++;
-	height = map_data->map_height;
-	free(map_data);
-	return (height);
-}
-
-int get_map_width(char **map)
-{
-	t_create_map *map_data;
-	int width;
-
-	map_data = ft_calloc(1, sizeof(t_create_map));
-	if (!map_data)
-		return (0);
-	while (map[0][map_data->map_width])
-		map_data->map_width++;
-	map_data->map_width--;
-	width = map_data->map_width;
-	free(map_data);
-	return (width);
-}
-
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
-	{
-		if (s1[i] != s2[i])
-			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
-		i++;
-	}
-	if (i < n)
-		return ((unsigned char) s1[i] - (unsigned char) s2[i]);
-	return (0);
 }
