c572ab5c967cf9fcd106158e09e4235f8a13d387
vivaz-ca | HOME <insanecoderbr06@gmail.com>
2025-06-11 08:22:55 +0100

    create map ready 100%

--- so_long/create_map.c
+++ so_long/create_map.c
@@ -6,34 +6,34 @@
 /*   By: vivaz-ca <vivaz-ca@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 17:36:08 by vivaz-ca          #+#    #+#             */
-/*   Updated: 2025/06/04 15:02:38 by vivaz-ca         ###   ########.fr       */
+/*   Updated: 2025/06/11 06:24:40 by vivaz-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	make_map(t_create_map *newMap, t_mlx_data *data)
+void	make_map(t_create_map *map, t_mlx_data *data)
 {
-	while (newMap->new_map[newMap->map_height])
+	while (map->new_map[map->map_height])
 	{
-		newMap->map_width = 0;
-		while (newMap->new_map[newMap->map_height][newMap->map_width])
+		map->map_width = 0;
+		while (map->new_map[map->map_height][map->map_width])
 		{
-			if (newMap->new_map[newMap->map_height][newMap->map_width] == '1')
-				draw_map(data, newMap, "./textures/tijolinho.xpm");
-			else if (newMap->new_map[newMap->map_height][newMap->map_width] == '0')
-				draw_map(data, newMap, "./textures/carpet.xpm");
-			else if (newMap->new_map[newMap->map_height][newMap->map_width] == 'E')
-				draw_map(data, newMap, "./textures/house.xpm");
-			else if (newMap->new_map[newMap->map_height][newMap->map_width] == 'P')
+			if (map->new_map[map->map_height][map->map_width] == '1')
+				draw_map(data, map, "./textures/tijolinho.xpm");
+			else if (map->new_map[map->map_height][map->map_width] == '0')
+				draw_map(data, map, "./textures/carpet.xpm");
+			else if (map->new_map[map->map_height][map->map_width] == 'E')
+				draw_map(data, map, "./textures/house.xpm");
+			else if (map->new_map[map->map_height][map->map_width] == 'P')
 			{
-				so_long()->x = newMap->map_width;
-				so_long()->y = newMap->map_height;
+				so_long()->x = map->map_width;
+				so_long()->y = map->map_height;
 			}
-			else if (newMap->new_map[newMap->map_height][newMap->map_width] == 'C')
-				draw_map(data, newMap, "./textures/rato.xpm");
-			newMap->map_width++;
+			else if (map->new_map[map->map_height][map->map_width] == 'C')
+				draw_map(data, map, "./textures/rato.xpm");
+			map->map_width++;
 		}
-		newMap->map_height++;
+		map->map_height++;
 	}
 }
