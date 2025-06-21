25f1ce7c830c61c7908f82984c7969c45afe79d1
vivaz-ca <vivaz-ca@student.42lisboa.com>
2025-06-21 19:58:56 +0100

    still working on chunks

--- push_swap/chuncks.c
+++ push_swap/chuncks.c
@@ -6,7 +6,7 @@
 /*   By: vivaz-ca <vivaz-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 18:03:27 by vivaz-ca          #+#    #+#             */
-/*   Updated: 2025/06/21 18:16:12 by vivaz-ca         ###   ########.fr       */
+/*   Updated: 2025/06/21 19:58:11 by vivaz-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,52 +19,47 @@ int	get_chunk_size(int len)
 	return (len / 11);
 }
 
-void	sort_using_chunks(long *a, long *b, t_base_value *value)
+void	sort_using_chunks(long *stk_a, long *stk_b, t_base_value *value)
 {
-	int		count = value->stack->stack_a_length;
-	int		chunk_size = get_chunk_size(count);
-	long	*sorted = malloc(sizeof(long) * count);
-	int		i, j, chunk_end;
-
-	for (i = 0; i < count; i++)
-		sorted[i] = a[i];
-	sort_long_array(sorted, count);
-	i = 0;
-	while (i < count)
+	long	*sorted;
+	int		len = value->stack->stack_a_length;
+	static int		i;
+	int j, chunk_number;
+	
+	sorted= quick_sort(value);
+	while (i < len)
 	{
-		chunk_end = i + chunk_size;
-		while (value->stack->stack_a_length > 0 && i < chunk_end && i < count)
+		chunk_number = i + get_chunk_size(len);
+		while ((value->stack->stack_a_length > 0) && (i < chunk_number) && (i < len))
 		{
 			j = 0;
 			while (j < value->stack->stack_a_length)
 			{
-				if (a[j] >= sorted[i] && a[j] <= sorted[chunk_end - 1])
+				if (stk_a[j] >= sorted[i] && stk_a[j] <= sorted[chunk_number - 1])
 				{
 					if (j <= value->stack->stack_a_length / 2)
 						while (j-- > 0)
-							single_rotate(a, value->stack, 0); // ra
+							single_rotate(stk_a, value->stack, 0); // ra
 					else
 						while (j++ < value->stack->stack_a_length)
-							reverse_rotate(a, value->stack, 0); // rra
-
-					push_elements(a, b, value->stack, 1); // pb
+							reverse_rotate(stk_a, value->stack, 0); // rra
+					push_elements(stk_a, stk_b, value->stack, 1); // pb
 					break ;
 				}
 				j++;
 			}
 		}
-		i = chunk_end;
+		i = chunk_number;
 	}
 	while (value->stack->stack_b_length > 0)
 	{
-		int max_i = find_max_index(b, value->stack->stack_b_length);
+		int max_i = find_max_index(stk_b, value->stack->stack_b_length);
 		if (max_i <= value->stack->stack_b_length / 2)
 			while (max_i-- > 0)
-				single_rotate(b, value->stack, 1); // rb
+				single_rotate(stk_b, value->stack, 1); // rb
 		else
 			while (max_i++ < value->stack->stack_b_length)
-				reverse_rotate(b, value->stack, 1); // rrb
-		push_elements(b, a, value->stack, 0); // pa
+				reverse_rotate(stk_b, value->stack, 1); // rrb
+		push_elements(stk_b, stk_a, value->stack, 0); // pa
 	}
 }
-
