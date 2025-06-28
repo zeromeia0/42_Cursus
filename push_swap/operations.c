4c986555d1a2579a9e51bd688256522a13d62e73
vivaz-ca | HOME <insanecoderbr06@gmail.com>
2025-06-28 15:11:25 +0100

    added double operations + makefile

--- push_swap/operations.c
+++ push_swap/operations.c
@@ -6,7 +6,7 @@
 /*   By: vivaz-ca <vivaz-ca@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 10:46:18 by vivaz-ca          #+#    #+#             */
-/*   Updated: 2025/06/28 15:02:55 by vivaz-ca         ###   ########.fr       */
+/*   Updated: 2025/06/28 15:10:25 by vivaz-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,30 +61,16 @@ int	single_swap(long *stk_x, long len, int which)
 	return (1);
 }
 
-void	double_swap(t_stack *stack)
-{
-	if (stack->stack_a_length < 2)
-	{
-		ft_printf("Stack_a ta vazio\n");
-		return ;
-	}
-	if (stack->stack_b_length < 2)
-	{
-		ft_printf("Stack_b ta vazio\n");
-		return ;
-	}
-	single_swap(stack->stack_a, stack->stack_a_length, 0);
-	single_swap(stack->stack_b, stack->stack_b_length, 1);
-	ft_printf("ss\n");
-}
-
 void	single_rotate(long *stk_x, t_stack *stack, int which)
 {
 	long	i;
 	long	temp;
 	long	len;
 
-	len = which == 1 ? stack->stack_a_length : stack->stack_b_length;
+	if (which == 1)
+		len = stack->stack_a_length;
+	else
+		len = stack->stack_b_length;
 	if (len < 1)
 	{
 		printf("len pequena");
@@ -104,30 +90,16 @@ void	single_rotate(long *stk_x, t_stack *stack, int which)
 		ft_printf("rb\n");
 }
 
-void	double_rotate(t_stack *stack)
-{
-	if (stack->stack_a_length < 2)
-	{
-		ft_printf("Stack_a ta vazio\n");
-		return ;
-	}
-	if (stack->stack_b_length < 2)
-	{
-		ft_printf("Stack_b ta vazio\n");
-		return ;
-	}
-	single_rotate(stack->stack_a, stack, 0);
-	single_rotate(stack->stack_b, stack, 1);
-	ft_printf("rr\n");
-}
-
 void	reverse_rotate(long *stk_x, t_stack *stack, int which)
 {
 	int		i;
 	long	temp;
 	long	len;
 
-	len = (which == 0) ? stack->stack_a_length : stack->stack_b_length;
+	if (which == 0)
+		len = stack->stack_a_length;
+	else
+		len = stack->stack_b_length;
 	if (len < 2)
 		return ;
 	temp = stk_x[len - 1];
@@ -142,11 +114,4 @@ void	reverse_rotate(long *stk_x, t_stack *stack, int which)
 		ft_printf("rra\n");
 	else
 		ft_printf("rrb\n");
-}
-
-void	double_reverse_rotate(t_stack *stack)
-{
-	reverse_rotate(stack->stack_a, stack, 0);
-	reverse_rotate(stack->stack_b, stack, 1);
-	ft_printf("rrr\n");
 }
