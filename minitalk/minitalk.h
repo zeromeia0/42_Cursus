931825c7848bd82bdcdbfcfdae30aeae39bca254
vivaz-ca <vivaz-ca@student.42lisboa.com>
2025-06-30 16:34:26 +0100

    started minitalk

---
+++ minitalk/minitalk.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vivaz-ca <vivaz-ca@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 22:06:40 by vivaz-ca          #+#    #+#             */
+/*   Updated: 2025/06/30 16:30:54 by vivaz-ca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <limits.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <strings.h>
+# include <unistd.h>
+
+void	ft_putstr(char *str);
+
+#endif
